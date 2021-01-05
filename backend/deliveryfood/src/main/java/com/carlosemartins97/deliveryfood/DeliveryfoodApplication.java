82518ef42b7b38a73e9b4d385b483511660ffcaa
Carlos Martins <carlosemartins97@gmail.com>
2021-01-04 22:55:50 -0300

    Project created.

---
+++ backend/deliveryfood/src/main/java/com/carlosemartins97/deliveryfood/DeliveryfoodApplication.java
@@ -0,0 +1,13 @@
+package com.carlosemartins97.deliveryfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryfoodApplication.class, args);
+	}
+
+}
---
+++ backend/deliveryfood/src/test/java/com/carlosemartins97/deliveryfood/DeliveryfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosemartins97.deliveryfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
