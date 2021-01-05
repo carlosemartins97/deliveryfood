0f0f465b513cdd360243a686fc0fc3c867cd45f6
Carlos Martins <carlosemartins97@gmail.com>
2021-01-05 01:10:19 -0300

    GET /orders

---
+++ backend/src/main/java/com/carlosemartins97/deliveryfood/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.carlosemartins97.deliveryfood.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.carlosemartins97.deliveryfood.dto.OrderDTO;
+import com.carlosemartins97.deliveryfood.entities.Order;
+import com.carlosemartins97.deliveryfood.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
