4e2c03c49ae92ad305022a6510ba199739714a4d
Carlos Martins <carlosemartins97@gmail.com>
2021-01-08 14:41:48 -0300

    fix errors

--- front-web/src/Orders/OrderSummary.tsx
+++ front-web/src/Orders/OrderSummary.tsx
@@ -6,7 +6,7 @@ type Props = {
     onSubmit: () => void;
 }
 
-function OrderSummary({amount, totalPrice, onSubmit: onSubmit}: Props) {
+function OrderSummary({amount, totalPrice, onSubmit}: Props) {
     return (
         <div className="order-summary-container">
             <div className="order-summary-content">
